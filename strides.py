6cc621d7d045c7dda5f30b62613d95a32c46d16c
Christine Richards <cricha88@uwo.ca>
2024-06-16 12:20:08 -0400

    Integrate Strava API

--- strides.py
+++ strides.py
@@ -2,16 +2,30 @@ import streamlit as st
 import requests
 import base64
 import os
+import time
+import dotenv
 
-# Replace these with your client ID and client secret
-client_id = os.environ.get('CLIENT_ID')
-client_secret = os.environ.get('CLIENT_SECRET')
+dotenv_file = dotenv.find_dotenv()
+dotenv.load_dotenv(dotenv_file, override=True)
+
+# SPOTIFY ACCESS TOKENS
+client_id_spotify = os.environ.get('CLIENT_ID_MYSPOT')
+client_secret_spotify = os.environ.get('CLIENT_SECRET_MYSPOT')
+
+# STRAVA ACCESS TOKENS
+client_id_strava = os.environ.get('CLIENT_ID_MYSTRIDE')
+client_secret_strava = os.environ.get('CLIENT_SECRET_MYSTRIDE')
+access_token_strava = os.environ.get('INITIAL_ACCESS_TOKEN_MYSTRIDE')
+refresh_token_strava = os.environ.get('INITIAL_REFRESH_TOKEN_MYSTRIDE')
+expires_at = os.environ.get('TOKEN_EXPIRY_MYSTRIDE')
+
+print(expires_at)
 
 # Function to get access token
-def get_access_token(client_id, client_secret):
+def get_spotify_access_token(client_id_spotify, client_secret_spotify):
     url = 'https://accounts.spotify.com/api/token'
     headers = {
-        'Authorization': 'Basic ' + base64.b64encode((client_id + ':' + client_secret).encode('utf-8')).decode('utf-8')
+        'Authorization': 'Basic ' + base64.b64encode((client_id_spotify + ':' + client_secret_spotify).encode('utf-8')).decode('utf-8')
     }
     data = {
         'grant_type': 'client_credentials'
@@ -45,6 +59,49 @@ def search_song(access_token, song_name):
     else:
         return None
 
+
+def refresh_strava_access_token(client_id, client_secret, refresh_token):
+    response = requests.post(
+        url='https://www.strava.com/oauth/token',
+        data={
+            'client_id': client_id_strava,
+            'client_secret': client_secret_strava,
+            'grant_type': 'refresh_token',
+            'refresh_token': refresh_token_strava
+        }
+    )
+    
+    tokens = response.json()
+    
+    # Update environment variables
+    os.environ['INITIAL_ACCESS_TOKEN_MYSTRIDE'] = tokens['access_token']
+    os.environ['INITIAL_REFRESH_TOKEN_MYSTRIDE'] = tokens['refresh_token']
+    os.environ['TOKEN_EXPIRY_MYSTRIDE'] = str(tokens['expires_at'])
+    dotenv.set_key(dotenv_file, "INITIAL_ACCESS_TOKEN_MYSTRIDE", os.environ['INITIAL_ACCESS_TOKEN_MYSTRIDE'])
+    dotenv.set_key(dotenv_file, "INITIAL_REFRESH_TOKEN_MYSTRIDE", os.environ['INITIAL_REFRESH_TOKEN_MYSTRIDE'])
+    dotenv.set_key(dotenv_file, "TOKEN_EXPIRY_MYSTRIDE", os.environ['TOKEN_EXPIRY_MYSTRIDE'])
+    
+    return tokens['access_token'], tokens['refresh_token'], int(tokens['expires_at'])
+
+
+def get_activities_data(expires_at, client_id_strava, client_secret_strava, access_token_strava, refresh_token_strava):
+    current_time = int(time.time())
+    expires_at = int(expires_at)
+    if current_time >= expires_at:
+        print("Access token has expired, refreshing token...")
+        access_token_strava, refresh_token_strava, expires_at = refresh_strava_access_token(client_id_strava, client_secret_strava, refresh_token_strava)   
+    else:
+        print("Access token is still valid.")
+
+    # Use the access token to get athlete data
+    endpoint = f"https://www.strava.com/api/v3/athlete/activities?after=1717215795&access_token={access_token_strava}"
+    response = requests.get(endpoint)
+    
+    return response.json()
+
+
+
+
 def main():
     st.title("Match My Stride")
 
@@ -57,10 +114,16 @@ def main():
         song_name = user_input
         
         # Get access token
-        access_token = get_access_token(client_id, client_secret)
+        spotify_access_token = get_spotify_access_token(client_id_spotify, client_secret_spotify)
         
         # Example usage
-        song_details = search_song(access_token, song_name)
+        song_details = search_song(spotify_access_token, song_name)
+
+        # Get activities data
+        activities = get_activities_data(expires_at, client_id_strava, client_secret_strava, access_token_strava, refresh_token_strava)
+
+        if activities:
+            st.write(activities)
 
         # Display the result
         if song_details:
